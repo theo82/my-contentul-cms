f68115904d0a7205379e71374b674bb04ebb6c1b
theo82 <theoaristi53@gmail.com>
2023-12-01 21:46:12 +0200

    Fetch Projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,9 @@
-import './fetchProjects'
+import { useFetchProjects } from './fetchProjects'
 import React from 'react'
 
 const Projects = () => {
+  const { loading, projects } = useFetchProjects()
+  console.log(loading, projects)
   return <h2>Projects</h2>
 }
 
--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { createClient } from 'contentful'
 
 const client = createClient({
@@ -7,6 +7,29 @@ const client = createClient({
   accessToken: 'xU_1i3BfI_3B1yT54bsBOlBcWU8kOhguTSYGtgH56P8',
 })
 
-client
-  .getEntries({ content_type: 'projects' })
-  .then((response) => console.log(response))
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' })
+      const projects = response.items.map((item) => {
+        const { url, title, image } = item.fields
+        const id = item.sys.id
+        const img = image?.fields?.file?.url
+        return { title, url, id, img }
+      })
+      setProjects(projects)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  return { loading, projects }
+}
